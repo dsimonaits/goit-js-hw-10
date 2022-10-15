c24a43a16a3a6848e2ca79bc0da796a0af33b568
Deniss Simonaits <dsimonaits@gmail.com>
2022-10-16 00:30:42 +0300

    css update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,7 @@ export default function fetchCountries(name) {
   const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
   return fetch(url).then(response => {
     if (!response.ok) {
-      throw new error(response.status);
+      throw new Error(response.status);
     }
     return response.json();
   });
